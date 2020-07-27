3e12a125113666ffaf56ff9d60e6a68d390e0a4b
drogba <pulawskk@gmail.com>
2020-07-27 21:51:04 +0200

    Close #3 update User class

--- src/main/java/com/pulawskk/dburger/domain/User.java
+++ src/main/java/com/pulawskk/dburger/domain/User.java
@@ -1,4 +1,20 @@
 package com.pulawskk.dburger.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_generator")
+    @SequenceGenerator(name = "users_id_generator", sequenceName = "users_id_sequence", allocationSize = 1)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
 }
