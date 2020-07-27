a1f40c37702b70c9bb68e5da2e2ebde8611a1eb6
drogba <pulawskk@gmail.com>
2020-07-27 22:36:08 +0200

    #5 add MapStruct dependency and UserMapper interface

---
+++ src/main/java/com/pulawskk/dburger/api/v1/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.pulawskk.dburger.api.v1.mapper;
+
+import com.pulawskk.dburger.api.v1.model.UserDto;
+import com.pulawskk.dburger.domain.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto userToUserDto(User user);
+}
