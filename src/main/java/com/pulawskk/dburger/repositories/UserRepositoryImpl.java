91f3d29891bd6753bda80bf986f4142a947c2ffd
drogba <pulawskk@gmail.com>
2020-07-27 22:40:19 +0200

    #4 change EntityManager to EntityManagerFactory

--- src/main/java/com/pulawskk/dburger/repositories/UserRepositoryImpl.java
+++ src/main/java/com/pulawskk/dburger/repositories/UserRepositoryImpl.java
@@ -3,10 +3,11 @@ package com.pulawskk.dburger.repositories;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 @Repository
 public class UserRepositoryImpl {
     @PersistenceUnit
-    EntityManager entityManager;
+    EntityManagerFactory entityManagerFactory;
 }
