1a6f608163d36bce9a0cd625bc61434df883ca0f
drogba <pulawskk@gmail.com>
2020-07-28 23:03:22 +0200

    close #13 limit result when is possibility to get more users than one, using UserRepositoryCustom

--- src/main/java/com/pulawskk/dburger/repositories/UserRepositoryCustom.java
+++ src/main/java/com/pulawskk/dburger/repositories/UserRepositoryCustom.java
@@ -1,4 +1,7 @@
 package com.pulawskk.dburger.repositories;
 
+import com.pulawskk.dburger.domain.User;
+
 public interface UserRepositoryCustom {
+    User findUserByLastName(String lastName);
 }
--- src/main/java/com/pulawskk/dburger/repositories/UserRepositoryImpl.java
+++ src/main/java/com/pulawskk/dburger/repositories/UserRepositoryImpl.java
@@ -1,13 +1,25 @@
 package com.pulawskk.dburger.repositories;
 
+import com.pulawskk.dburger.api.v1.model.UserDto;
+import com.pulawskk.dburger.domain.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 
 @Repository
 public class UserRepositoryImpl {
     @PersistenceUnit
     EntityManagerFactory entityManagerFactory;
+
+    User findUserByLastName(String lastName) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String sql = "SELECT u FROM User u where u.lastName =:lastName";
+        Query query = entityManager.createQuery(sql);
+        query.setMaxResults(1);
+        query.setParameter("lastName", lastName);
+        return (User) query.getSingleResult();
+    }
 }
