78276b8071efc7d562a5fe0f1b695e39f67ad0c0
drogba <pulawskk@gmail.com>
2020-07-28 18:11:28 +0200

    #10 add userUrl field to User

--- src/main/java/com/pulawskk/dburger/api/v1/model/UserDto.java
+++ src/main/java/com/pulawskk/dburger/api/v1/model/UserDto.java
@@ -1,5 +1,6 @@
 package com.pulawskk.dburger.api.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,7 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String email;
+
+    @JsonProperty(value = "user_url")
+    private String userUrl;
 }
