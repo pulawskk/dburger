bd3073b0894f7ca10e8274b9c8fe5fb7f3aa7644
drogba <pulawskk@gmail.com>
2020-08-02 14:43:11 +0200

    #22 implement controler's method to display all orders on get request

---
+++ src/main/java/com/pulawskk/dburger/controllers/v1/OrderController.java
@@ -0,0 +1,25 @@
+package com.pulawskk.dburger.controllers.v1;
+
+import com.pulawskk.dburger.api.v1.model.OrderListDto;
+import com.pulawskk.dburger.services.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(OrderController.ORDER_BASE_URL)
+@CrossOrigin
+public class OrderController {
+    public final static String ORDER_BASE_URL = "/api/v1/orders";
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public OrderListDto displayAllOrders() {
+        return orderService.findAllOrdersDto();
+    }
+}
