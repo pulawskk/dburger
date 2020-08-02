dff7976bf0fe7f6382e83e7a5da7bb1f7cd6ec0b
drogba <pulawskk@gmail.com>
2020-08-02 13:50:57 +0200

    #22 add order dtos objects

---
+++ src/main/java/com/pulawskk/dburger/api/v1/model/OrderDto.java
@@ -0,0 +1,24 @@
+package com.pulawskk.dburger.api.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderDto {
+    private Long id;
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZIP;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+    private LocalDateTime placedAt;
+
+    @JsonProperty(value = "user_url")
+    private String orderUrl;
+}
