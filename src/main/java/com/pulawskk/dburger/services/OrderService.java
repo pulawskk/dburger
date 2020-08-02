69eda6106ca8a98a8001eacdfeb2e8c6917436c4
drogba <pulawskk@gmail.com>
2020-08-02 13:56:55 +0200

    #22 add order service layer

---
+++ src/main/java/com/pulawskk/dburger/services/OrderService.java
@@ -0,0 +1,18 @@
+package com.pulawskk.dburger.services;
+
+import com.pulawskk.dburger.api.v1.model.OrderDto;
+import com.pulawskk.dburger.api.v1.model.OrderListDto;
+
+public interface OrderService {
+    OrderListDto findAllOrdersDto();
+
+    OrderDto findOrderById(Long id);
+
+    OrderDto createNewOrder();
+
+    OrderDto updateOrder(Long id, OrderDto orderDto);
+
+    OrderDto patchOrder(Long id, OrderDto orderDto);
+
+    void deleteOrder(Long id);
+}
---
+++ src/main/java/com/pulawskk/dburger/services/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package com.pulawskk.dburger.services;
+
+import com.pulawskk.dburger.api.v1.model.OrderDto;
+import com.pulawskk.dburger.api.v1.model.OrderListDto;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public OrderListDto findAllOrdersDto() {
+        return null;
+    }
+
+    @Override
+    public OrderDto findOrderById(Long id) {
+        return null;
+    }
+
+    @Override
+    public OrderDto createNewOrder() {
+        return null;
+    }
+
+    @Override
+    public OrderDto updateOrder(Long id, OrderDto orderDto) {
+        return null;
+    }
+
+    @Override
+    public OrderDto patchOrder(Long id, OrderDto orderDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long id) {
+
+    }
+}
