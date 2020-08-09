a1ee28f24ab1ce298ec8bf95b8541cf7f8968908
drogba <pulawskk@gmail.com>
2020-08-09 12:52:29 +0200

    #23 fix enum mappings

--- src/main/java/com/pulawskk/dburger/api/v1/mapper/IngredientMapper.java
+++ src/main/java/com/pulawskk/dburger/api/v1/mapper/IngredientMapper.java
@@ -3,6 +3,7 @@ package com.pulawskk.dburger.api.v1.mapper;
 import com.pulawskk.dburger.api.v1.model.IngredientDto;
 import com.pulawskk.dburger.domain.Ingredient;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,7 +11,9 @@ public interface IngredientMapper {
 
     IngredientMapper INSTANCE = Mappers.getMapper(IngredientMapper.class);
 
+    @Mapping(source = "ingredientType", target = "type")
     IngredientDto ingredientToIngredientDto(Ingredient ingredient);
 
+    @Mapping(source = "type", target = "ingredientType")
     Ingredient ingredientDtoToIngredient(IngredientDto ingredientDto);
 }
